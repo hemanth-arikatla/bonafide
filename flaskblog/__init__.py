6ee0d015ff672881c788ffaf41adee408d8f61d6
Hemanth Kumar Arikatla <hemanth.chowdary1314@gmail.com>
2021-03-14 03:53:41 -0400

    Add files via upload

---
+++ flaskblog/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import os
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '4a12400e9609c9a006cc481a0cdcfc5d'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from flaskblog import routes
