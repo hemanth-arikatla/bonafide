87ffb15a8dece906726ffaaf87c164eb3a3d1d38
Hemanth Kumar Arikatla <hemanth.chowdary1314@gmail.com>
2021-03-15 10:02:08 -0400

    Update run.py

--- run.py
+++ run.py
@@ -2,5 +2,10 @@ from flaskblog import app
 
 
 
+'''if __name__ == '__main__':
+    app.run(debug=True)'''
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv('PORT'))
+    print("Starting app on port %d" % port)
+    app.run(debug=False, port=port, host='0.0.0.0')
